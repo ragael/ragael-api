6cfba38d2eca3bf4fe58808eaf190e281f66dbda
ragael <ragael@gmail.com>
2021-04-21 12:26:09 -0300

    gethtml com axios e playwright

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
+var axios = require('axios').default
+var { chromium } = require('playwright');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/gethtml', async function (req, res, next) {
+  let url = req.query.url || '';
+  if (url == '') {
+    res.status(400);
+  } else {
+    try {
+      var html = await axios.get(url);
+      res.send({ html: html.data });
+    } catch (error) {
+      var browser = await chromium.launch();
+      var page = await browser.newPage();
+      await page.goto(url);
+      var html = await page.content();
+      res.send({ html: html });
+    }
+  }
 });
 
 module.exports = router;
